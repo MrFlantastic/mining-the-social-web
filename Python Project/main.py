623679108cbb13e4bfb27ae1c709ab5967fb705e
Flanny <matthewflanagan6@gmail.com>
2021-08-19 14:47:12 -0400

    setting api access

--- Python Project/main.py
+++ Python Project/main.py
@@ -1,2 +1,15 @@
 ### This is the file to test our Twitter API scripts
-import
+import tweepy
+import pandas as pd
+
+customer_key = 'MJsP5vZzgMEBQJMViZ0I6R1bd'
+customer_secret = 'IbhVV9yvGdBw9fjsce5aSZq5kazXnBR29fqLCD7H1WjzcpH03u'
+access_token = '486977981-9vqd1VTICsY4i1HOFC0eBkvU2JUUXuxmjMS6nwwa'
+access_token_secret = 'MTNvx5Fuula5Bqwo0iEqP0XMXB5nSgWipbTxtuAw4H4e3'
+
+auth = tweepy.OAuthHandler(customer_key, customer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
+
+me = api.get_user(screen_name = 'MrFlantastic')
+me.id 
